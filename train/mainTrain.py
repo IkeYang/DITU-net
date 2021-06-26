2cf0271db987a33eace5a198e18d9ab7f8a1206a
IkeYang <42018430+IkeYang@users.noreply.github.com>
2021-06-26 23:02:54 +0800

    Update mainTrain.py

--- train/mainTrain.py
+++ train/mainTrain.py
@@ -1,6 +1,6 @@
 #Author:ike yang
 from trainUnet import trainUnet
-dirP = r"..\SCADA Data Synthesis\TrainData"
+dirP = r"..\SCADADataSynthesis\TrainData"
 name='DEAndADE'
 trainUnet(dirP,name,setLen=4000,epochs=20)
 
